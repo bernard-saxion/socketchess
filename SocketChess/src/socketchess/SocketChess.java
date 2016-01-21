bc78f4a96fdc30e1377621c599cb6a02f55a1797
saxion <saxion@ranger.upwhere.me.>
2016-01-21 15:29:33 +0000

    Main class applet, with a button to start the client and to start the server.

--- SocketChess/src/socketchess/SocketChess.java
+++ SocketChess/src/socketchess/SocketChess.java
@@ -5,17 +5,62 @@
  */
 package socketchess;
 
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author espinosa baeza & bernard
  */
-public class SocketChess {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class SocketChess extends Applet implements ActionListener
+{
+    private final Button asclient = new Button("Play as white (client)");
+    private final Button asserver = new Button("Play as black (server)");
     
+    private final Label addressl = new Label("Server address:");
+    private final Label portl = new Label("Server port:");
+    
+    private final TextField addressf = new TextField();
+    private final TextField portf = new TextField();
+    
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    @Override
+    public void init()
+    {
+        setBackground(Color.lightGray);
+        
+        add(addressl);
+        addressf.setText("000.000.000.000");
+        add(addressf);
+        add(portl);
+        portf.setText("5500");
+        add(portf);
+        
+        asclient.addActionListener(this);
+        add(asclient);
+        asserver.addActionListener(this);
+        add(asserver);
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getSource()==asclient)
+        {
+            
+        }
+        if(ae.getSource()==asserver)
+        {
+            
+        }
+    }
 }
